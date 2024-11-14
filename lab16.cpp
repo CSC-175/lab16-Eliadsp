e897a8f1b2edc52db02bc04df1238bd3df93e43e
eliadspraggins <eliad@umich.edu>
2024-11-14 14:36:14 -0500

    Attempt 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,16 @@
 // Code to implement the gcf function goes here
+#include <iostream>
+using namespace std;
+int gcf(int a, int b) {
+    // Ensure a and b are positive
+    a = abs(a);
+    b = abs(b);
+
+    // Euclidean algorithm
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
